a1c62c8888763b68b2bf73ff4017e7997dc1ba4e
Joel Fuentes <joel.fuentes@uci.edu>
2015-01-19 01:51:57 -0800

    assigment 1 (a to d) implemented and tested

--- src/assigment1/TestAssigment.java
+++ src/assigment1/TestAssigment.java
@@ -7,25 +7,35 @@ import assigment1.a.Utilities;
 import assigment1.a.Pair;
 import assigment1.b.WordFrequencies;
 import assigment1.c.TwoGrams;
+import assigment1.d.Palindromes;
 
 public class TestAssigment {
 	
 	public static void main(String[] args){
 		//test part A
 		System.out.println("Part A");
-		File file= new File("file.txt");
+		File file= new File("file2.txt");
 		List<String> list =Utilities.tokenizeFile(file);
 		Utilities.print(list);
 		
-		System.out.println("Part B");
 		//test part B
-		List<Pair<String, Integer>> listFrecuencies=WordFrequencies.computeWordFrequencies(list);
-		WordFrequencies.print(listFrecuencies);
+		System.out.println();
+		System.out.println("Part B");
+		List<Pair<String, Integer>> listFrequencies=WordFrequencies.computeWordFrequencies(list);
+		WordFrequencies.print(listFrequencies);
 		
-		System.out.println("Part C");
 		//test part C
+		System.out.println();
+		System.out.println("Part C");
 		List<Pair<String, Integer>> listFrequenciesGrams=TwoGrams.computeTwoGramFrequencies(list);
 		TwoGrams.print(listFrequenciesGrams);
+		
+		
+		//Test part D
+		System.out.println();
+		System.out.println("Part D");
+		List<Pair<String, Integer>> listFrequenciesPalindromes=new Palindromes().computePalindromeFrequencies(list);
+		Palindromes.print(listFrequenciesPalindromes);
 	}
 
 }
